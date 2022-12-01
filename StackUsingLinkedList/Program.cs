6965bd0c6902d85b3005ecf4f5b970072a904b10
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-12-01 16:16:33 +0700

    add method main & add switch, case 1,2,3,4

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -59,11 +59,45 @@ namespace StackUsingLinkedList
                 Console.WriteLine();
             }
         }
-    }
-    internal class Program
-    {
         static void Main(string[] args)
         {
+            Stacks s = new Stacks();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your choice: ");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+                    case '3':
+                        s.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+                        break;
+                }
+            }
         }
     }
 }
