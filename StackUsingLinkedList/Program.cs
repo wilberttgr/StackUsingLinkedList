224fcef38428e11a4b2de664b6454b58d8b8b752
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-12-01 15:49:37 +0700

    add class Node & method Node

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace StackUsingLinkedList
 {
+    class Node
+    {
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
+        {
+            info = i;
+            next = n;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
