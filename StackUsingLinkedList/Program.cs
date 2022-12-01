eb921078b4aefa84dd3b45af66412ec2a53487de
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2022-12-01 15:55:24 +0700

    add method push and bool empty

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -23,6 +23,22 @@ namespace StackUsingLinkedList
         {
             top = null;
         }
+        bool empty()
+        {
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + "pushed.");
+        }
     }
     internal class Program
     {
